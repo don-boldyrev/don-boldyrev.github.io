ce1ba67188af18bf799ba3bf3ac00cb8ea180cd9
Aleksandr Boldyrev <don.boldyrev2014@gmail.com>
2021-04-20 18:10:07 +0300

    first commit

---
+++ precache-manifest.af74be67ceeb57844f182d49768e7418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6269d639796464951bbdf9d68b124386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077615a831edcd5b56a7",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cc817a0cf923955fd3d0",
+    "url": "/static/css/main.459c67fb.chunk.css"
+  },
+  {
+    "revision": "077615a831edcd5b56a7",
+    "url": "/static/js/2.dbe84923.chunk.js"
+  },
+  {
+    "revision": "2db80ee6e43bb57ab10aefd739542bd4",
+    "url": "/static/js/2.dbe84923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc817a0cf923955fd3d0",
+    "url": "/static/js/main.46d252ab.chunk.js"
+  },
+  {
+    "revision": "c08ec8307b6209861342",
+    "url": "/static/js/runtime-main.b8b08aae.js"
+  }
+]);
